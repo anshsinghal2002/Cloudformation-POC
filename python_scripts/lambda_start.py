3eadd6ffddf8196ca1f863f37e4394cdb658e1e8
Ansh Singhal <anshsinghal@uniqus.com>
2024-11-04 09:59:30 -0500

    lambda changes

--- python_scripts/lambda_start.py
+++ python_scripts/lambda_start.py
@@ -1,6 +1,17 @@
 import json
 import boto3
-def lambda_handler(event,context):
-    target = event["crawlername"]
-    glueclient = boto3.client('glue')
-    glueclient.start_crawler(Name=target)
+
+def lambda_handler(event, context):
+    try:
+        target = event["crawlername"]
+        glueclient = boto3.client('glue')
+        glueclient.start_crawler(Name=target)
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Crawler started successfully')
+        }
+    except Exception as e:
+        return {
+            'statusCode': 500,
+            'body': json.dumps(str(e))
+        }
